f8cc141a98ddd99828dbed626b5a42ed95d53442
thiagobrx <thiagolink500@gmail.com>
2024-06-11 17:51:21 -0300

    teste

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                 <div class="collapse navbar-collapse flex-grow-0 text-center" id="navbarNav">
                     <ul class="navbar-nav position-relative"">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">HOME</a>
+                            <a class="nav-link" href="#">HOM</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">CONTATO</a>
