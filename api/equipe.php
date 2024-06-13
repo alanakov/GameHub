c1c515cab66b7ebdbcbcb2b6970636616e80eb37
Alana Kovalski de Angeli <angelikovalski@gmail.com>
2024-06-13 16:15:57 -0300

    página de equipe feita, precisa ser atualizado as fotos e informações dos membros da equipe

--- api/equipe.php
+++ api/equipe.php
@@ -4,9 +4,9 @@
 
     $path = "https://www.professorburnes.com.br/stem/images/";
 
-    $equipe[1] = array("nome"=>"Alana Kovalski de Angeli","cargo"=>"Desenvolvedor de Games / CEO / Diretora de Arte","foto"=>"imagens/alanaKov.jpg");
-    $equipe[2] = array("nome"=>"Gabriel Menechini","cargo"=>"Desenvolvedor de Games","foto"=>"gabrielMechi.jpeg");
-    $equipe[3] = array("nome"=>"Gabriel S. Speciam","cargo"=>"Desenvolvedor de Games","foto"=>"{$path}anakin.jpg");
-    $equipe[4] = array("nome"=>"Lucas Brasso","cargo"=>"Desenvolvedor de Games / Fisiculturista nas horas vagas","foto"=>"{$path}leia.jpg");
+    $equipe[1] = array("nome"=>"ai nao sei o que","cargo"=>"Desenvolvedor de Games","foto"=>"imagens/gabrielMechi.jpeg");
+    $equipe[2] = array("nome"=>"Gabriel Menechini","cargo"=>"Desenvolvedor de Games","foto"=>"imagens/gabrielMechi.jpeg");
+    $equipe[3] = array("nome"=>"Gabriel S. Speciam","cargo"=>"Desenvolvedor de Games","foto"=>"imagens/gabrielMechi.jpeg");
+    $equipe[4] = array("nome"=>"Lucas Brasso","cargo"=>"Desenvolvedor de Games / Fisiculturista nas horas vagas","foto"=>"imagens/gabrielMechi.jpeg");
 
     echo json_encode($equipe);
--- paginas/equipe.php
+++ paginas/equipe.php
@@ -1,5 +1,45 @@
+<?php
+//file_get_contents funcionar
+$arrContextOptions = array(
+    "ssl" => array(
+      "verify_peer" => false,
+      "verify_peer_name" => false,
+    ),
+  );
+  
 
+$urlEquipe = "https://localhost/Project2/api/equipe.php";
+$dadosApi = file_get_contents($urlEquipe, false, stream_context_create($arrContextOptions));
+$dadosEquipeApi = json_decode($dadosApi);
+?>
 
-$urlBanners = "https://localhost/Project2/api/banners.php";
-$dadosApi = file_get_contents($urlBanners, false, stream_context_create($arrContextOptions));
-$dadosBannersApi = json_decode($dadosApi);
+<p class="text-white text-center mt-5 fs-3 fw-bold">CONHEÇA NOSSA EQUIPE!</p>
+
+<div class=" m-5 d-flex justify-content-center">
+      <div class="card rounded-4" style="width: 20rem; background-color: #101010;">
+        <p class="bg-danger text-white p-2 m-0 rounded-end-3 position-absolute d-flex float-end">LÍDER</p>
+        <img src="imagens/alanaKov.jpg" class="card-img-top rounded-top-4" alt="Alana Kovalski">
+        <div class="card-body">
+          <p class="card-text text-white">Meowtopia</p>
+          <p class="card-text" style="color: #707070">Desenvolvedor de Games • CEO • Diretora de Arte</p>
+        </div>
+      </div>
+  </div>
+
+<div class="cards container d-md-flex justify-content-center">
+    <?php
+    foreach ($dadosEquipeApi as $equipe) {
+      ?>
+      <div class="container p-3 d-flex justify-content-center">
+        <div class="card rounded-4" style="width: <?= $equipe->width ?>; background-color: #101010;">
+            <img src="<?= $equipe->foto ?>" class="card-img-top rounded-top-4" alt="...">
+            <div class="card-body">
+              <p class="card-text text-white"><?= $equipe->nome ?> </p>
+              <p class="card-text" style="color: #707070"><?= $equipe->cargo ?></p>
+            </div>
+        </div>
+      </div>
+      <?php
+    }
+    ?>
+  </div>
